b204749d0e1ca7669b1a2127b8b86d2ff7e5efb4
apianta <44927262+apianta@users.noreply.github.com>
2019-01-30 19:38:20 -0500

    Add new files

---
+++ krause_brothers_flooring/workspace/main/KrauseBrothersFlooring-master/KrauseBrothersFlooring-master/krause_brothers_flooring/workspace/main/js/app.js
@@ -0,0 +1,11 @@
+var facebookIcon = document.getElementById("facebook");
+var phoneIcon = document.getElementById("phone");
+var emailIcon = document.getElementById("email");
+var reAnimate = document.getElementById("animate");
+
+console.log('hello');
+
+reAnimate.addEventListener('click', function(event) {
+	
+});
+
